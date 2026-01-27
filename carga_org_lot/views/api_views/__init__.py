1facc24a4d7787ae230e55062d51975710c9fe89
ProjetcsGPP <gpp@seger.es.gov.br>
2026-01-27 09:13:54 -0300

    ♻️ Update: Add new ViewSets to api_views exports
    - Export LotacaoJsonOrgaoViewSet
    - Export TokenEnvioCargaViewSet
    - Export auxiliary read-only ViewSets
    - Maintain backward compatibility

--- carga_org_lot/views/api_views/__init__.py
+++ carga_org_lot/views/api_views/__init__.py
@@ -8,6 +8,8 @@ from .patriarca_api import *
 from .organograma_api import *
 from .lotacao_api import *
 from .carga_api import *
+from .lotacao_json_api import *
+from .token_api import *
 
 __all__ = [
     # Dashboard
@@ -16,9 +18,19 @@ __all__ = [
     'upload_organograma',
     'upload_lotacao',
     
-    # ViewSets
+    # ViewSets Principais
     'PatriarcaViewSet',
     'OrganogramaVersaoViewSet',
     'LotacaoVersaoViewSet',
     'CargaPatriarcaViewSet',
+    
+    # ViewSets Novos
+    'LotacaoJsonOrgaoViewSet',
+    'TokenEnvioCargaViewSet',
+    
+    # ViewSets Auxiliares (Read-Only)
+    'StatusProgressoViewSet',
+    'StatusCargaViewSet',
+    'TipoCargaViewSet',
+    'StatusTokenEnvioCargaViewSet',
 ]
