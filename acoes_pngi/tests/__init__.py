36c71a2b29f88b92e9273bab95fc7e5a89ca6f5c
ProjetcsGPP <gpp@seger.es.gov.br>
2026-02-10 10:20:17 -0300

    test(acoes_pngi): Criar estrutura de testes unitários Python

--- acoes_pngi/tests/__init__.py
+++ acoes_pngi/tests/__init__.py
@@ -1,1 +1,10 @@
-# acoes_pngi/tests/__init__.py
+"""
+Testes Unitários - Ações PNGI
+
+Estrutura de testes:
+- test_models.py: Testes dos modelos
+- test_serializers.py: Testes dos serializers
+- test_permissions.py: Testes do sistema de permissões
+- test_api_views.py: Testes das views API
+- test_web_views.py: Testes das views web
+"""
